3da8295a0086621da88e2c60e81d929704c704b0
DreamerKing <dreamingking@live.cn>
2019-09-21 01:48:40 +0800

    58 句子最后一个单词长度

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ *
+ * [58] 最后一个单词的长度
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let len = 0; 
+    let l = s.length;
+    let start = l - 1;
+    let char = s.charAt(start);
+    while (char == ' ') {
+      start--;
+      char = s.charAt(start);
+    }
+   
+    while (start >= 0 && char && char != ' ') {
+        start--;
+        char = s.charAt(start);
+        len++;
+    }
+    return len;
+};
+
+
+
+
